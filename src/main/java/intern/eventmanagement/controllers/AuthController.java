b469170a29234d3038a8d4bf758458f11d00820f
trinhnguyen1810 <trinhnguyen@uni.minerva.edu>
2023-08-14 10:11:43 +0700

    fix register

--- src/main/java/intern/eventmanagement/controllers/AuthController.java
+++ src/main/java/intern/eventmanagement/controllers/AuthController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +27,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public String login(
-            @ModelAttribute("user") User user,
-            BindingResult result,
-            Model model
+            @ModelAttribute("user") User user
     ) {
         Logger logger = LoggerFactory.getLogger(AuthController.class);
         User foundUser = userRepository.findByEmail(user.getEmail());
@@ -42,8 +39,7 @@ public class AuthController {
         if (foundUser != null && passwordEncoder.matches(user.getPassword(), foundUser.getPassword())) {
             return "redirect:/home";
         } else {
-
-            model.addAttribute("error", true);
+            //model.addAttribute("error", true);
             return "redirect:/home";
         }
     }
